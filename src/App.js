4a434c69408ca49a6b0e3be45014f1d1b828fe4f
Sadia Malik <sadiaabida156@gmail.com>
2023-10-22 15:54:41 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// import React from 'react';
+
+
+// import CardProvider from './Component/CardProvider';
+// import AddCard from './Component/AddCard';
+// import CardDisplay from './Component/CardDisplay';
+
+
+// function App(){
+//   return(
+//    <div className='col-lg-12'>
+
+// <div className='p-0 mt-5 d-flex align-items-center justify-content-center flex-column'>
+
+// <h1 className='mb-5 text-success'> useContext</h1>
+
+// <CardProvider>
+//  <div >
+//    <h1>Add Card</h1>
+//    <AddCard />
+//  </div>
+//  <div>
+//    <h1>Card Display</h1>
+//    <CardDisplay />
+//  </div>
+
+// </CardProvider>
+
+
+ 
+// </div>
+//    </div>
+//   );
+// }
+// export default App;
+
+import './App.css';
+import Hero from './components/Hero';
+
+function App() {
+  return <Hero />;
+}
+
+export default App;
